59b07c174bfcfca9b8e2b40d0d72686fcf6129b5
Yana Zdravkova <yana.zdravkovaa@gmail.com>
2021-03-18 16:11:22 +0200

    Refactor

--- BusinessDirectory/src/app/components/item-view/item-view.component.ts
+++ BusinessDirectory/src/app/components/item-view/item-view.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Business } from 'src/app/models/business';
 import { ActivatedRoute } from '@angular/router';
 import { DataService } from 'src/app/services/data.service';
-import { Address } from 'src/app/models/address';
+import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-item-view',
@@ -13,8 +13,8 @@ export class ItemViewComponent implements OnInit {
 
   private businessId!: string;
   public business!: Business;
-  public nearbyPlaces!: Business[];
-  public columnsToDisplay = ['name', 'description'];
+  public nearbyPlacesToDisplay: Business[] = [];
+  readonly NEARBY_PLACES_COUNT = 4;
   
   constructor(
     private route: ActivatedRoute,
@@ -22,14 +22,25 @@ export class ItemViewComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
-      this.businessId = params['id'];
-
-      this.dataService.getItems().subscribe(items => {
-        this.business = items.find(b => b.id === this.businessId);
-        this.nearbyPlaces = items.filter(b => b.address?.country === this.business.address?.country && b.id !== this.business.id).slice(0, 4);
+    combineLatest([this.route.params, this.dataService.getItems()]).subscribe(
+      ([routerParams, businessList]) => {
+        this.businessId = routerParams['id'];
+        this.business = businessList.find(b => b.id === this.businessId);
+        const allNearbyPlaces = businessList.filter(place => this.isNearbyPlaceTo(place, this.business));
+        this.nearbyPlacesToDisplay = allNearbyPlaces.slice(0, this.NEARBY_PLACES_COUNT);
       });
-    });
   }
 
+  private isNearbyPlaceTo(placeToCheck: Business, business: Business): boolean {
+    return placeToCheck.address?.country === business.address?.country &&
+           placeToCheck.id !== business.id;
+  }
+
+  get formattedBusinessAddress(): string {
+    return this.business?.address?.number + ' ' + this.business?.address?.street + ', ' + this.business?.address?.zip;
+  }
+
+  public formatNearbyPlaceDescription(place: Business): string {
+    return place.address?.street + ', ' + place.address?.city + ' ' + place.address?.zip;
+  }
 }
